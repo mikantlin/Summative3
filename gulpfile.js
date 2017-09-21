e046a7811a8a3eee8d9e2fa12f861c922a169786
mikantlin <lines0812@gmail.com>
2017-09-21 22:15:27 +1200

    set up projects list and about section

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@
  
 let gulp = require('gulp');
 let sass = require('gulp-sass');
+let postcss = require('gulp-postcss');
 let sourcemaps = require('gulp-sourcemaps');
- 
+let autoprefixer = require('autoprefixer');
+
 gulp.task('sass', function () {
   return gulp.src('./sass/styles.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer()]))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./css'));
 });
--- js/script.js
+++ js/script.js
@@ -1,6 +1,53 @@
+const behanceAPI = 'TJWj9OP9YyUJxO7X1cD2Dovr6e5NeOWJ';
+const username = 'Aria_Baro';
+
 $(function() {
 
   // jquery coding inside this function
-  
+  let userURL = `https://api.behance.net/v2/users/${username}?client_id=${behanceAPI}`;
 
+  // prepare templates for user info and projects
+  let userInfoHTML = $('#user-info-template').text();
+  let userInfoTemplate = Template7(userInfoHTML).compile();
+
+  let projectListHTML = $('#project-list-template').text();
+  let projectListTemplate = Template7(projectListHTML).compile();  
+  
+  $.ajax({
+    cache: true,
+    url: userURL,
+    dataType: 'jsonp',
+    success: (res) => {
+
+      let data = res.user;
+
+      let user = {
+        name: data.display_name,
+        location: data.location,
+        descriptions: data.sections,
+        headshot: Object.values(data.images).pop()
+      };
+
+      let userInfo = userInfoTemplate(user);
+      $('.user').append(userInfo);
+      
+      let projectsURL = `https://api.behance.net/v2/users/${username}/projects?client_id=${behanceAPI}`;
+      console.log(projectsURL);
+
+      $.ajax({
+        cache: true,
+        url: projectsURL,
+        dataType: 'jsonp',
+        success: (res) => {
+          let projects = res.projects;
+
+          projects.forEach((project) => {
+            let listItem = projectListTemplate(project);
+            $('.projects').append(listItem);            
+          });
+        }
+      });
+
+    }
+  });  
 });
